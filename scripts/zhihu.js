a81dc909e4b36602968c092da442476975e47558
s-desktop <36028655+s-w1tch@users.noreply.github.com>
2025-08-23 16:02:13 +0800

    1.1.1

--- scripts/zhihu.js
+++ scripts/zhihu.js
@@ -2,11 +2,15 @@
  * @Author: sw1tch
  * @Date: 2025-08-23 14:09:25
  * @LastEditors: sw1tch
- * @LastEditTime: 2025-08-23 14:13:15
+ * @LastEditTime: 2025-08-23 15:56:35
  *
  * Copyright (c) 2025 by sw1tch, All Rights Reserved.
  */
-let jsonResponse = JSON.parse($response.body);
-jsonResponse.data = [];
-// console.log(JSON.stringify(jsonResponse));
-$done(JSON.stringify(jsonResponse));
+let url = $request.url;
+let rsp = JSON.parse($response.body);
+if (url.includes("/recommendations")) {
+    rsp.data = [];
+} else if (url.includes("/answers")) {
+    rsp.ad_info = {};
+}
+$done(JSON.stringify(rsp));
