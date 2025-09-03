8600498794c3c0c11715cbb9c964d0cbd2209658
s-desktop <36028655+s-w1tch@users.noreply.github.com>
2025-09-03 18:24:40 +0800

    1.1.11

--- scripts/zhihu.js
+++ scripts/zhihu.js
@@ -1,5 +1,5 @@
 // const rsp = require('../debug.json');
-// let url = '';
+// let url = 'v4/questions';
 let url = $request.url;
 let rsp = JSON.parse($response.body);
 if (url.includes("/recommendations")) {
@@ -8,13 +8,24 @@ if (url.includes("/recommendations")) {
 } else if (url.includes('topstory/recommend')) {
     console.log('-首页推荐广告');
     rsp.data = rsp.data.filter(item => !item.hasOwnProperty('adjson'));
-}
-if (rsp.hasOwnProperty('ad_info')) {
+} else if (url.includes('v4/questions')) {
+    // if (rsp.hasOwnProperty('ad_info')) {
     console.log('-问题中间广告');
     rsp.ad_info = {
         "position": 3,
         "ad": {},
         "adjson": ""
     };
+    let new_data_list = [];
+    for (let temp_data of rsp.data) {
+        let item_real_id = temp_data.url.split('/').slice(-1)[0];
+        if (String(temp_data.id) != item_real_id) {
+            console.log(`-ID越界: url: ${item_real_id} -> id: ${temp_data.id}`)
+            temp_data.id = item_real_id;
+        }
+        new_data_list.push(temp_data);
+    }
+    rsp.data = new_data_list
+    // }
 }
 $done(JSON.stringify(rsp));
